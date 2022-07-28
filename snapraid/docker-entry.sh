0c79b7a9eea7bca9f09a8c6695183440b2b7ba0b
hcwhan <hcwhan@gmail.com>
2022-07-28 11:34:06 +0800

    update

--- snapraid/docker-entry.sh
+++ snapraid/docker-entry.sh
@@ -3,11 +3,19 @@
 # in /config. In reality, both files should be edited manually before running this
 # container to ensure correct operation.
 
+if [ ! -L /etc/crontabs/root ] && [ -f /etc/crontabs/root ]; then
+    rm /etc/crontabs/root
+fi
 # test for /etc/snapraid.conf being a file and not a link, delete if file.
 if [ ! -L /etc/snapraid.conf ] && [ -f /etc/snapraid.conf ]; then
     rm /etc/snapraid.conf
 fi
 
+
+if [ ! -f /config/snapraid-cron ]; then
+    echo "No snapraid-cron found. You must add snapraid-cron before running this container."
+    exit 1
+fi
 # test if snapraid.conf is in /config, copy from /defaults/snapraid.conf.example if not.
 if [ ! -f /config/snapraid.conf ]; then
     echo "No config found. You must configure SnapRAID before running this container."
@@ -18,10 +26,13 @@ if [ ! -f /config/snapraid-runner.conf ]; then
     exit 1
 fi
 
-# test if link is made between /etc/snapraid.conf and /config/snapraid.conf, make if not
+
+if [ ! -L /etc/crontabs/root ]; then
+    ln -s /config/snapraid-cron /etc/crontabs/root
+fi
 if [ ! -L /etc/snapraid.conf ]; then
     ln -s /config/snapraid.conf /etc/snapraid.conf
 fi
 
-/usr/sbin/crond -d 6 -c /etc/crontabs -f 
 
+/usr/sbin/crond -d 6 -c /etc/crontabs -f
